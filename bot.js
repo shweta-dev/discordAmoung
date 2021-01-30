44edd731ce850b897382f97f5301a09175a76cbd
shweta agarwal <shweta.agarwal@and.digital>
2021-01-30 12:21:08 +0000

    swarits code

--- bot.js
+++ bot.js
@@ -1,24 +1,29 @@
-let Discord = require('discord.js');
-let client = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('ready', () => {
-  //// client.user.setPresence({activity: { name:"Among you" }, status: "available"} )
+  console.log('Online.');
 });
 
-client.on('message', message => {
-  if (message.content == 'ping') {
-    message.channel.send('pongsss');
-  }
+client.on('message', async message => {
+  if (message.author.bot) return;
+  let prefix = 'LS!'; //customized. you can change it whatever you want.
+  if (!message.content.startsWith(prefix)) return; // use this so your bot will be only executed with prefix.
 
-  if (message.content === 'embed') {
-    let embed = new Discord.MessageEmbed()
-      .setTitle('Hi I am title of embed')
-      .setDescription('I am the description')
-      .setColor('RED')
-      .setFooter('I am the footer');
+  let args = message.content
+    .slice(prefix.length)
+    .trin()
+    .split(/ +/g);
+  let msg = message.content.toLowerCase();
+  let cmb = args.shift().toLowerCase();
+  // Type all the messages you want over here. Use the command given below for example.
 
-    message.channel.send(embed);
-  }
+  if (msg.startsWith(prefix + `ping`)) {
+    // Instead of ping, you can type any command. Example, help.
+    message.channel.send('pong'); //What the bot will respond with
+
+    // Make sure all messages commands are under this client.
+  } // easy way.
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN2);
